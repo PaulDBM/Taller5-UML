d85b022aa9e61014e489d443190027889e4d4d3b
Paul Douglas Bustos Mite <paudobus@espol.edu.ec>
2020-11-24 09:51:02 -0500

    SistemaClinico_DC añadido

---
+++ SistemaClinico_DC/src/main/java/ec/edu/espol/modelos/Persona.java
@@ -0,0 +1,26 @@
+package ec.edu.espol.modelos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author paul.bustos
+ */
+public class Persona {
+
+    protected String usuario;
+    protected String clave;
+    protected String nombre;
+    protected String apellido;
+    protected String cedula;
+    protected String direccion;
+    protected Date fechaNac;
+    
+    public boolean login(){
+        return true;
+    }
+    
+    public boolean logOut(){
+        return true;
+    }
+}
